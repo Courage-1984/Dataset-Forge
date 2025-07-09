1545d575c7f902bc519f61786d416b7d326e55ab
Courage-1984 <dieter.dieter.no1@gmail.com>
2025-07-09 20:48:05 +0200

    cleaned up dataset_forge root

--- dataset_forge/combine.py
+++
@@ -1,8 +0,0 @@
-from dataset_forge.dataset_ops import DatasetCombiner
-from dataset_forge.utils.history_log import log_operation
-
-
-def combine_datasets(src1, src2, dest):
-    combiner = DatasetCombiner()
-    combiner.run()
-    log_operation("combine", f"Combined {src1} and {src2} into {dest}")
